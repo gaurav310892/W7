8fd093082236818c14e75ee50e91aac165b607f0
Gaurav310892 <djfhkdfj@hjj.com>
2019-10-21 02:27:20 +0000

    First commit Message

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,13 @@
+read -p "Enter your Emailaddress for git account :" emailA
+read -p "Paste the Path of dir that needs to be committed" pathA
+read -p "Enter the URL of url git branch" urlA
+echo "email is $emailA"
+echo "Path set for uplading $pathA"
+echo "URL selected for uploading is $urlA"
+git config --global user.email $emailA
+break
+
+
 git init
 git add . 
 git commit -m "First commit Message"
